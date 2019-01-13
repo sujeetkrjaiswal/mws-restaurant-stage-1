329832eae968241492c8083f638ad3540c74edb9
Sujeet Kumar Jaiswal <Sujeet_Jaiswal@outlook.com>
2019-01-13 20:10:53 +0530

    updated images path from for gh-pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -160,7 +160,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return `/img/${restaurant.photograph}`;
+    return `img/${restaurant.photograph}`;
   }
 
   /**
